fd634cc8f5cf038d925a4eafe9f318818d361a17
Mario <mjvinti@gmail.com>
2018-03-07 20:53:18 -0500

    Fixed keys

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+	mongoURI: 'mongodb://mario:mario@ds259778.mlab.com:59778/storybooks-dev',
+	googleClientID: '105094170719-d93o785lvfl1a522ngbjbeei2l732pk3.apps.googleusercontent.com',
+	googleClientSecret: 'h4wbCY-kEAdOD80huzGsvPvP'
+}

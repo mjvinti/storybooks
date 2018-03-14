1fdb75251d671382a3958f60ab7a8fd4bf9d395d
Mario <mjvinti@gmail.com>
2018-03-13 21:24:16 -0400

    Started UI

--- app.js
+++ app.js
@@ -1,22 +1,59 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 const passport = require('passport');
 
-const app = express();
-const port = process.env.PORT || 3000;
+// Load User Model
+require('./models/User');
 
 // Passport Config
 require('./config/passport')(passport);
 
 // Load Routes
+const index = require('./routes/index');
 const auth = require('./routes/auth');
 
-// Use Routes
-app.use('/auth', auth);
+// Load Keys
+const keys = require('./config/keys');
 
-app.get('/', (req, res) => {
-	res.send('It works');
+// Map global promises
+mongoose.Promise = global.Promise;
+// Mongoose Connect
+mongoose.connect(keys.mongoURI)
+	.then(() => console.log('MognoDB Connected'))
+	.catch(err => console.log(err));
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Handlebars middleware
+app.engine('handlebars', exphbs({
+	defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+app.use(cookieParser());
+app.use(session({
+	secret: 'secret',
+	resave: false,
+	saveUninitialized: false
+}));
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Set global vars
+app.use((req, res, next) => {
+	res.locals.user = req.user || null;
+	next();
 });
+
+// Use Routes
+app.use('/', index);
+app.use('/auth', auth);
 
 app.listen(port, () => {
 	console.log(`Server started on ${port}`);
